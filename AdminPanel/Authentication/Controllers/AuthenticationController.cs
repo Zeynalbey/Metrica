5efe3820e7b67c2840eb111b855f1d0b01ee979a
Zeynalbey <zeynaleg@code.edu.az>
2022-11-13 15:44:24 +0400

    Some Views created.

---
+++ AdminPanel/Authentication/Controllers/AuthenticationController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminPanel.Authentication.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult RePassword()
+        {
+            return View();
+        }
+
+        public IActionResult LockScreen()
+        {
+            return View();
+        }
+
+        public IActionResult Error404()
+        {
+            return View();
+        }
+        public IActionResult Error500()
+        {
+            return View();
+        }
+    }
+}
